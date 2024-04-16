add4f834b99596bc315140a8d5b8c02d7f01e66f
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-04-16 17:52:14 +0200

    feat: add typescript types

---
+++ src/index.d.ts
@@ -0,0 +1,19 @@
+declare type Release = () => void;
+declare type Acquire = (resolve: (release: Release) => void) => void;
+declare type Lock = () => Promise<Release>;
+
+interface LockInterface extends Lock {
+  isLocked: () => boolean;
+}
+
+declare function createLock(slots?: number): LockInterface;
+
+declare type WithLock = <T>(fn: () => Promise<T>) => Promise<T>;
+
+interface WithLockInterface extends WithLock {
+  isLocked: () => boolean;
+}
+
+declare function withLock(opts: number): WithLockInterface;
+
+export { withLock, createLock };
