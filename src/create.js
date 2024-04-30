6b665aa281c1eb7c94520075122cf8c25f595a57
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-04-30 16:54:47 +0200

    feat: use DoublyLinkedList
    instead of stack promises into an array, it uses a double linked list.
    
    The main advantage is `queue.shift` hurts the performance when for big arrays O(n), while a double linked list keep access time constant O(1)

--- src/create.js
+++ src/create.js
@@ -1,11 +1,41 @@
 'use strict'
 
+class Node {
+  constructor (data) {
+    this.data = data
+  }
+}
+
+class DoublyLinkedList {
+  enqueue (data) {
+    const node = new Node(data)
+    if (!this.head) {
+      this.head = node
+      this.tail = node
+    } else {
+      node.prev = this.tail
+      this.tail.next = node
+      this.tail = node
+    }
+  }
+
+  dequeue () {
+    if (!this.head) return
+    const data = this.head.data
+    this.head = this.head.next
+    if (this.head) this.head.prev = undefined
+    else this.tail = undefined
+    return data
+  }
+}
+
 module.exports = (slots = 1) => {
-  const queue = []
+  const queue = new DoublyLinkedList()
 
   const release = () => {
     ++slots
-    if (queue.length > 0) queue.shift()()
+    const fn = queue.dequeue()
+    if (fn !== undefined) fn()
   }
 
   const acquire = resolve => {
@@ -16,7 +46,7 @@ module.exports = (slots = 1) => {
   const lock = () =>
     new Promise(resolve =>
       lock.isLocked()
-        ? queue.push(acquire.bind(null, resolve))
+        ? queue.enqueue(acquire.bind(null, resolve))
         : acquire(resolve)
     )
 
